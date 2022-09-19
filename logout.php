ef3238a34b8e59557ee18139d1da8588e1cf392e
PrajwalBhat123 <75970070+PrajwalBhat123@users.noreply.github.com>
2022-09-19 19:27:11 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-        //session_start();
-        session_destroy();
-        //$_SESSION['username'] = NULL;
-        unset($_SESSION['username']);
-        header('location:index.html');
-        exit;
-        
-?>
